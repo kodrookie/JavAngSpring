53cf63335625d32bda2c1366142cf66c67c0fc09
Linn <kodrookie@gmail.com>
2020-11-16 23:00:26 +0100

    Testar göra en pull request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+/** HALLOOOO*/
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
